dcc6afe54591b45be2e534f84128b328e5fafbdd
ThomasLaforge <thomas.laforge.38@gmail.com>
2024-06-23 00:10:31 +0200

    simon game style

--- src/routes/SimonGame.tsx
+++ src/routes/SimonGame.tsx
@@ -1,4 +1,5 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
+import "../styles/_simon.scss";
 
 const COLORS = ["red", "blue", "green", "yellow"];
 
@@ -46,40 +47,72 @@ export default function SimonGame() {
     });
   }, [colors, nbErrors, hasVoyelle]);
 
+  const handleRemoveLast = useCallback(() => {
+    setColors(colors.slice(0, -1));
+  }, [colors]);
+
   return (
     <div className="simon-game">
-      <h2>Le numéro de série contient une voyelle ?</h2>
-      <input
-        type="checkbox"
-        name="has-voyelle"
-        id="has-voyelle"
-        checked={hasVoyelle}
-        onClick={() => setHasVoyelle(!hasVoyelle)}
-      />
-
-      <h2>Nombre d'erreurs ?</h2>
-      <input
-        type="number"
-        name="nb-errors"
-        id="nb-errors"
-        min={0}
-        max={2}
-        value={nbErrors}
-        onChange={(e) => setNbErrors(parseInt(e.target.value))}
-      />
-
-      <h2>Sequence</h2>
-
-      <h3>Add a color</h3>
+      <h1 className="simon-game-title">Simon Game</h1>
+      <div className="simon-game-options">
+        <div className="option option-has-voyelle">
+          <label className="option-label">
+            Le numéro de série contient une voyelle :
+          </label>
+          <input
+            className="option-input"
+            type="checkbox"
+            name="has-voyelle"
+            id="has-voyelle"
+            checked={hasVoyelle}
+            onClick={() => setHasVoyelle(!hasVoyelle)}
+          />
+        </div>
+        <div className="option option-nb-errors">
+          <label className="option-label">Nombre d'erreurs :</label>
+          <input
+            className="option-input"
+            type="number"
+            name="nb-errors"
+            id="nb-errors"
+            min={0}
+            max={2}
+            value={nbErrors}
+            onChange={(e) => setNbErrors(parseInt(e.target.value))}
+          />
+        </div>
+      </div>
 
       <div className="colors-form" style={{ display: "flex" }}>
         {COLORS.map((color, key) => (
           <div
             key={key}
-            className="color-form-elt"
+            className={
+              "color-form-elt" +
+              (color === "yellow" ? " color-form-elt-yellow" : "")
+            }
             onClick={() => setColors([...colors, color])}
-            style={{ backgroundColor: color, width: "50px", height: "50px" }}
-          />
+            style={{ backgroundColor: color }}
+          >
+            +
+          </div>
+        ))}
+      </div>
+
+      <h3>Actuelle</h3>
+
+      <div className="sequence-render" style={{ display: "flex" }}>
+        {colors.map((c, k) => (
+          <div
+            key={k}
+            className={
+              "sequence-elt" + (c === "yellow" ? " sequence-elt-yellow" : "")
+            }
+            onClick={() => k === colors.length - 1 && handleRemoveLast()}
+            style={{ backgroundColor: c }}
+          >
+            {k === colors.length - 1 ? "-" : ""}
+          </div>
         ))}
       </div>
 
@@ -91,19 +124,15 @@ export default function SimonGame() {
         reset
       </button>
 
-      <h3>Actuelle</h3>
-
-      <div className="sequence-render" style={{ display: "flex" }}>
-        {colors.map((c, k) => (
-          <div key={k} className="sequence-elt" style={{ backgroundColor: c, width: "50px", height: "50px" }} />
-        ))}
-      </div>
-
       <h2>A cliquer</h2>
 
       <div className="solution-render" style={{ display: "flex" }}>
         {colorsToClick.map((c, key) => (
-          <div key={key} className="color-to-click" style={{ backgroundColor: c, width: "50px", height: "50px" }} />
+          <div
+            key={key}
+            className="color-to-click"
+            style={{ backgroundColor: c }}
+          />
         ))}
       </div>
     </div>
